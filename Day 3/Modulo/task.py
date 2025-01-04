448136cc9650e350e8a0b3c8a7373419144d54bc
Sonish S <sonishstark@gmail.com>
2025-01-03 20:31:27 -0500

    Day_3_(2)

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,12 @@
+def main():
+    number = int(input("Enter an number: "))
+    odd_even(number)
+
+def odd_even(n):
+    if n % 2 == 0:
+        print(f"{n} is an Even number")
+    else:
+        print(f"{n} is an Odd number")
+
+main()
+
