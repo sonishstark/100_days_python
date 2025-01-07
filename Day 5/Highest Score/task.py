d020471e704017b739f01e9a12b7da8db05f42ff
Sonish Stark <sonishstark@gmail.com>
2025-01-07 10:06:05 -0500

    Day_5

--- Day 5/Highest Score/task.py
+++ Day 5/Highest Score/task.py
@@ -1,2 +1,18 @@
 student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
-print(range(1, 10))
+
+#inbuilt function to add
+toal_exam_score = sum(student_scores)
+sum = 0
+
+#standard way adding using loops
+for score in student_scores:
+    sum += score
+
+print(sum)
+
+large = 0
+for score in student_scores:
+    if score > large:
+        large = score
+
+print(large)
