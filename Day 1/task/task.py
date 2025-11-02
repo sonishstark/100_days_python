678888efcde40a4122e34f75a4b5a615980e3fc4
Sonish S <52243863+sonishstark@users.noreply.github.com>
2025-11-01 23:37:51 -0400

    Day 1

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,14 @@
-#testing pull request
-#coming from windows
+name = input("what is you name?")
+print(name)
+
+name2 = "Jack"
+print(name2)
+
+#assigning different values for the same variable
+name2 = "Adams"
+print(name2)
+
+#regardless of assigning different value to same variable it takes last assigned one
+length = len(name2)
+print(length)
+ 
