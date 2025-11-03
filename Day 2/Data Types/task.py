66a035cc266d184233ede0fc22c8ceba071ca088
Sonish S <52243863+sonishstark@users.noreply.github.com>
2025-11-02 19:04:40 -0500

    Day 2, 2

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,20 @@
-len("Hello")
+#the process is called subscripting
+print("Hello"[4])
+# - does it in backwards
+print("Hello"[-2])
+
+#even numbers are considered as strings when inside double quotes
+print("123" + "456")
+
+#Integer = whole number
+print(123 + 456)
+
+#large Integers always split them with underscore not comma
+print(123_456_789)
+
+#float = floating point numbers
+print(3.1456)
+
+#Boolean
+print(True)
+print(False)
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,16 @@
-len(12345)
+len("1234")
+
+#the type inbuilt function helps you identify what data type it is
+print(type("Hello"))
+print(type(123))
+print(type(12.3))
+print(type(True))
+
+
+#type conversion or casting
+int("123")
+
+
+
+
+
