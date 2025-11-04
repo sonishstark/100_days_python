0ca832d0ad47c0d1559e0d5be20538cbd8af9065
Sonish S <sonishstark@gmail.com>
2025-11-04 14:00:10 -0500

    Day 2 done

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,23 @@
 bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi))
+
+print(round(bmi))
+
+#two decimal places
+print(round(bmi,2))
+
+score = 0
+
+score += 1
+print(score)
+
+print("you score is " + str(score))
+
+score1 = 2
+height =1.8
+is_winning = True
+#fstring
+print(f"Your score is = {score} and your height is {height} so winning is {is_winning}")
+
