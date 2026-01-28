9756b6f29b34e282a962909a4453326c02340494
Sonish S <52243863+sonishstark@users.noreply.github.com>
2026-01-28 14:12:15 -0500

    Day 6 robo jump project

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -7,3 +7,26 @@ def addition(a,b):
 
 my_dob()
 addition(3,4)
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def climb():
+    turn_left()
+    while front_is_clear():
+        move()
+        if not at_goal():
+            while not wall_on_right():
+                turn_right()
+                move()
+
+
+while not at_goal():
+    if wall_in_front():
+        climb()
+    else:
+        move()
