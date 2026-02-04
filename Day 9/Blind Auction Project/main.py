b1e9a1347c556b285d5066bbd89155f8b828ecde
Sonish S <sonishstark@gmail.com>
2026-02-04 14:48:55 -0500

    Day 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,5 +2,25 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+import art
+print(art.logo)
+
+def highest_bidder(bidders):
+    highest_value = max(bidders, key=bidders.get)
+    print(f"The highest bidder is {highest_value} with a bid of ${bidders[highest_value]}")
 
 
+more_bidders = True
+bidders ={}
+while more_bidders:
+    name = input("What is your name?:")
+    bid_amount = int(input("What is your bid?:"))
+    bidders[name]= bid_amount
+    print(bidders)
+    other_bidders = input("Are there other bidders? Type 'Yes' or 'No': ").lower()
+    if other_bidders == "yes":
+        print("\n"*20)
+    elif other_bidders == "no":
+        more_bidders = False
+
+highest_bidder(bidders)
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,41 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+'''programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again"
+}
+
+print(programming_dictionary["Bug"])
+
+programming_dictionary["List"] = "Can store a number of elements"
+print(programming_dictionary)
+
+empty_dictionary = {}
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
+'''
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+student_grades ={}
+
+for key in student_scores:
+    if student_scores[key] >= 91 and  student_scores[key] <= 100:
+        student_grades[key] = "Outstanding"
+    elif student_scores[key] >= 81 and  student_scores[key] <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif student_scores[key] >= 71 and  student_scores[key] <= 80:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+for grades in student_grades:
+    print(student_grades[grades])
+
